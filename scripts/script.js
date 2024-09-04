86c4f3770df5e863c342057e0dd074e82758e867
LailaAlba <z1001@yandex.ru>
2024-09-04 20:09:05 +0300

    Добавляет верстку на проверку

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 const searchButton = document.querySelector('.search-button-brown');
 const modal = document.querySelector('.modal-container');
-const closeButton = document.querySelector('.close-button') ;
+const closeButton = document.querySelector('.close-button');
 
 searchButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    modal.classList.remove('modal-container-close')
+  evt.preventDefault();
+  modal.classList.remove('modal-container-close');
 });
 
 closeButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    modal.classList.add('modal-container-close')
+  evt.preventDefault();
+  modal.classList.add('modal-container-close');
 });
